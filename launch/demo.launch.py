c78d7f4f088f87a9cd3ba868e99247cc3ae25a9f
Salvatore Del Peschio <saviodp7@gmail.com>
2025-08-16 09:43:54 +0200

    default planner and fix launchers

--- launch/demo.launch.py
+++ launch/demo.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("x500", package_name="x500_moveit_config_2").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("x500", package_name="x500_moveit_config").to_moveit_configs()
     return generate_demo_launch(moveit_config)
